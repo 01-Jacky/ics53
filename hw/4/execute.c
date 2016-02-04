787f4b74f54ccef2dff6707df88d46e17195331b
Thomas <ttang1@uci.edu>
2016-02-04 14:38:36 -0800

    Finished hw4 part 1

--- hw/4/execute.c
+++ hw/4/execute.c
@@ -13,35 +13,43 @@ void parse(char* input, char** args)
 {
 	while (*input != '\0') 
 	{
-		while (*input == ' ' || *input == '\t')
+		while (*input == ' ' || *input == '\t' || *input == '\n')
 			*input++ = '\0';
-		*args++ = input;
-		while (*input != '\0' && *input != ' ' && *input != '\t')
+		*args = input;
+		while (*input != '\0' && *input != ' ' && *input != '\t' && *input != '\n')
 			input++;
+		if (strlen(input) == 0)
+			*args = NULL;
+		args++;
 	}
-	*args = 0;
+	*args = NULL;
 }
 
+void execute(char** args, char* en_path)
+{
+	if (execvp(args[0], args) < 0)
+	{
+		char* bin = strtok(en_path, ":");
+		char path[1024];
+		while (bin != NULL)
+		{
+			snprintf(path, sizeof(path), "%s/%s", bin, args[0]);
+			execvp(path, args);
+			bin = strtok(NULL, ":");
+		}
+	}
+}
 
 int main(int argc, char* argv[], char* envp[]) 
 {
-	char* en_path = getenv("PATH");
-	char* bin;
-	bin = strtok(en_path, ":");
-
 	char input[1024];
-	char* args[64];
 	fgets(input, 1024, stdin);
+
+	char* args[64] = {NULL};
 	parse(input, args);
-	printf("%s", args[0]);
-/*
-	while (bin != NULL)
-	{
-		printf("%s\n", bin);
-		snprintf(path, sizeof(path), "%s/%s", bin, args[0]);
-		execvp(path, "/bin");
-		bin = strtok(NULL, ":");
-	}
-*/
+	
+	char* en_path = getenv("PATH");
+	execute(args, en_path);
+
 	return 0;
 }
