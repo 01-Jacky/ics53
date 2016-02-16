14d7488a52d774c463b50a3c755c4d72c46d24e3
Thomas <ttang1@uci.edu>
2016-02-16 10:22:04 -0800

    more hw5

--- hw/5/hw_5/my_shell.c
+++ hw/5/hw_5/my_shell.c
@@ -1,8 +1,45 @@
+/*
+	Thomas Tang
+	82502633
+	ICS53 HW5
+	my_shell.c
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.H>
+#include <unistd.h>
+
+void parse()
+{
+	char ch;
+	while((ch = getchar() != EOF))
+	{
+		switch(ch)
+		{
+			case ' ':
+				break;
+			case '<':
+				break;
+			case '>':
+				break;
+			default:
+				break;
+		}
+	}
+
+}
+
+
+void shell()
+{
+	while(1)
+	{
+		fputs("% ", stdout);
+		parse();
+	}
+}
 
 int main()
-{
-
+{	
+	shell();
 }
